1c835a743e859154ff67337ddaf8e9bc203f5765
Hdcapoe <jamal223hd@gmail.com>
2023-11-12 11:41:52 -0500

    latest update3

--- fightingGame.js
+++ fightingGame.js
@@ -7,40 +7,113 @@ canvas.height = 800;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-    constructor(position) {
-        this.position = position;
-    }
-    draw() {
-        c.fillStyle = 'green'
-        c.fillRect(this.position.x, this.position.y, 30, 100)
-    }
-};
-
-const player = new Sprite({
-    x: 0,
-    y: 0
-});
-
-player.draw()
-console.log(player)
-
-function animate() {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    player.draw()
+	constructor(position) {
+		this.position = position;
+	}
+	draw() {
+		c.fillStyle = 'green';
+		c.fillRect(this.position.x, this.position.y, 30, 100);
+	}
 }
 
-animate()
+const player = new Sprite({
+	x: 0,
+	y: 0,
+});
 
+player.draw();
+console.log(player);
+
+let isMoving = false;
+
+const keys = {
+    f: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    e: {
+        pressed: false
+    }
+}
+
+function animate() {
+	window.requestAnimationFrame(animate);
+	c.fillStyle = 'black';
+	c.fillRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    
+    if (keys.f.pressed) {
+        player.position.x = 2
+    } else if (keys.s.pressed) {
+        player.position.x = -2
+    } else if (keys.d.pressed) {
+        player.position.y = 2
+    } else if (keys.e.pressed) {
+        player.position.y = -2
+    }
+}
+
+animate();
 
 window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'c':
-            player.position.y = 1
-            break;
+	if (!isMoving) {
+		isMoving = true;
+		switch (event.key) {
+			case 'f':
+				moveRight();
+				break;
+			case 's':
+				moveLeft();
+				break;
+			case 'd':
+				moveDown();
+				break;
+			case 'e':
+				moveUp();
+				break;
+		}
+	}
+});
+
+window.addEventListener('keyup', (event) => {
+    if (['f', 's', 'd', 'e'].includes(event.key)) {
+        isMoving = false;
     }
+});
 
-console.log(event.key)
-})
+function moveRight() {
+	if (isMoving) {
+		player.position.x += 2;
+		animate();
+		setTimeout(moveRight, 16);
+	}
+}
 
+function moveLeft() {
+	if (isMoving) {
+		player.position.x -= 2;
+		animate();
+		setTimeout(moveLeft, 16);
+	}
+}
+
+function moveDown() {
+	if (isMoving) {
+		player.position.y += 2;
+		animate();
+		setTimeout(moveDown, 16);
+	}
+}
+
+function moveUp() {
+	if (isMoving) {
+		player.position.y -= 2;
+		animate();
+		setTimeout(moveUp, 16);
+	}
+}
